8c3e9d40264aad6ce16fe1d56895c3e865dfd462
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-26 11:44:10 +0500

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import "@/styles/globals.css";
 
-import NextProgress from "../hooks/NextProgress";
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <NextProgress />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }

a0c2c60b3705e0c3c41a757b5af3d26aca4c6a93
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-27 01:58:36 +0500

    done with request limiter

--- utils/speedTestUtil.js
+++ utils/speedTestUtil.js
@@ -1,11 +1,22 @@
 const exec = require("speedtest-net");
+let lastRequestTime = 0;
+const requestInterval = 1000; // limit implemented and waited for while before make it capable of running
 async function performSpeedTest() {
+  const currentTime = Date.now();
+  const elapsedTimeSinceLastRequest = currentTime - lastRequestTime;
+  if (elapsedTimeSinceLastRequest < requestInterval) {
+    await new Promise((resolve) =>
+      setTimeout(resolve, requestInterval - elapsedTimeSinceLastRequest)
+    );
+  }
   try {
     const result = await exec();
+    lastRequestTime = Date.now();
     return result;
   } catch (error) {
     console.error("Error in test:", error);
-    return null;
+    throw new Error("Speed test failed");
   }
 }
+
 module.exports = { exec: performSpeedTest };

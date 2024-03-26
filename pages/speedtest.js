8b7db7f17bf215299b37a59c9c20d92817c0b552
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-27 00:30:28 +0500

    Update speedtest.js

--- pages/speedtest.js
+++ pages/speedtest.js
@@ -1,12 +1,11 @@
-// pages/SpeedTestPage.js
 import React, { useState } from "react";
-import { Button, CircularProgress } from "@mui/material";
 import axios from "axios";
+import { Button, CircularProgress } from "@mui/material";
 
 export default function SpeedTestPage() {
-  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [speedTestResult, setSpeedTestResult] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleRunSpeedTest = async () => {
     setIsLoading(true);
@@ -20,43 +19,39 @@ export default function SpeedTestPage() {
     }
   };
 
-  return (
-    <div>
-      <div className="container mx-auto mt-8 bg-white p-6 rounded-lg shadow-lg text-center">
-        <h1 className="text-3xl font-bold mb-4 text-blue-500">
-          {" "}
-          Real Speed Test
-        </h1>
-        <Button
-          className=" bg-slate-500"
-          variant="contained"
-          color="primary"
-          onClick={handleRunSpeedTest}
-          disabled={isLoading}
-          startIcon={
-            isLoading ? <CircularProgress size={20} color="inherit" /> : null
-          }
-        >
-          {isLoading ? "Running Test..." : "Run Speed Test"}
-        </Button>
-        {speedTestResult && (
-          <div>
-            <h2 className="text-2xl font-bold mt-8 mb-4">Speed Test Results</h2>
-            <p className="text-lg mb-2">
-              Ping: {speedTestResult.ping.latency} ms
-            </p>
-            <p className="text-lg mb-2">
-              Download Speed:{" "}
-              {(speedTestResult.download.bandwidth / 1024 / 1024) * 8} Mbps
-            </p>
-            <p className="text-lg mb-2">
-              Upload Speed:{" "}
-              {(speedTestResult.upload.bandwidth / 1024 / 1024) * 8} Mbps
-            </p>
-          </div>
-        )}
-        {error && <p className="text-red-500 mt-4">Error: {error}</p>}
+  const renderSpeedTestResults = () => {
+    if (!speedTestResult) return null;
+
+    return (
+      <div>
+        <h2 className="text-2xl font-bold mt-8 mb-4">Speed Test Results</h2>
+        <p className="text-lg mb-2">Ping: {speedTestResult.ping.latency} ms</p>
+        <p className="text-lg mb-2">
+          Download Speed: {speedTestResult.download.bandwidth} Mbps
+        </p>
+        <p className="text-lg mb-2">
+          Upload Speed: {speedTestResult.upload.bandwidth} Mbps
+        </p>
       </div>
+    );
+  };
+
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-4">Speed Test</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleRunSpeedTest}
+        disabled={isLoading}
+        startIcon={
+          isLoading ? <CircularProgress size={20} color="inherit" /> : null
+        }
+      >
+        {isLoading ? "Running Test..." : "Run Speed Test"}
+      </Button>
+      {renderSpeedTestResults()}
+      {error && <p className="text-red-500 mt-4">Error: {error}</p>}
     </div>
   );
 }
